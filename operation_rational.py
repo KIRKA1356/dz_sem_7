8d01cef14d52d6649b7016e7361a86a9b5d50709
KIRKA1356 <kgritsun@mail.ru>
2022-10-03 21:09:29 +0300

    11

---
+++ operation_rational.py
@@ -0,0 +1,32 @@
+'''
+Рациональные числа - числа с дробной частью
+Иницилизация рационального числа. Операции с числами находятся в модуле operation_complex чтобы не дублировать код
+'''
+x = 0
+y = 1
+
+
+def init(a, b):
+    global x
+    global y
+    x = float(a)
+    y = float(b)
+
+
+def summ():
+    return f'{x} + {y} = {x + y}'
+
+
+def sub():
+    return f'{x} - {y} = {x - y}' 
+
+
+def mult():
+    return f'{x} * {y} = {x * y}'
+
+
+def part():
+    try:
+        return f'{x} / {y} = {x / y}'
+    except ZeroDivisionError:
+        print('Вы делите на ноль. Так нельзя')
